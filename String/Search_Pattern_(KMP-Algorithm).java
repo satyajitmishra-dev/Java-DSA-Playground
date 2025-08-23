e18733dafa77f1fcc0cf814ee69f71b55468a7d5
Satyajit Mishra <143812895+satyajitmishra-dev@users.noreply.github.com>
2025-08-23 22:02:09 +0530

    Add Python code in Strings

--- String/Search_Pattern_(KMP-Algorithm).java
+++ String/Search_Pattern_(KMP-Algorithm).java
@@ -62,3 +62,53 @@ class Solution {
         return ans; // Return list of starting indices where pattern matched
     }
 }
+
+// In python
+class Solution:
+    def constructlps(self, lps, pat):
+        length = 0  # length of the previous longest prefix suffix
+        lps[0] = 0  # lps[0] is always 0
+
+        i = 1
+        while i < len(pat):
+            if pat[i] == pat[length]:
+                length += 1
+                lps[i] = length
+                i += 1
+            else:
+                if length != 0:
+                    length = lps[length - 1]
+                else:
+                    lps[i] = 0
+                    i += 1
+
+    def search(self, pat, txt):
+        ans = []
+        n = len(pat)
+        m = len(txt)
+
+        # Step 1: Construct the LPS array for the pattern
+        lps = [0] * n
+        self.constructlps(lps, pat)
+
+        # Step 2: Start matching using KMP algorithm
+        i = 0  # pointer for txt
+        j = 0  # pointer for pat
+
+        while i < m:
+            if txt[i] == pat[j]:
+                i += 1
+                j += 1
+
+                # If full pattern is matched
+                if j == n:
+                    ans.append(i - j)  # Add starting index of match
+                    j = lps[j - 1]     # Reset j using LPS to find next match
+            else:
+                # Mismatch after j > 0 characters matched
+                if j != 0:
+                    j = lps[j - 1]     # Backtrack in pattern
+                else:
+                    i += 1             # Move to next character in text
+
+        return ans  # Return list of starting indices where pattern matched
